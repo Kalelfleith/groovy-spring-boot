1e40f2288e564279690325083943d5299b6bdb46
kalelfleith <kalelfleith03@gmail.com>
2022-01-18 23:30:34 -0300

    backend

---
+++ backend/src/main/groovy/com/groovy/appDemo/AppDemoApplication.groovy
@@ -0,0 +1,13 @@
+package com.groovy.appDemo
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AppDemoApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AppDemoApplication, args)
+	}
+
+}
