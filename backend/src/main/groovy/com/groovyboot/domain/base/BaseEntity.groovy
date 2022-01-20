9f5218828bee33bdb250e4129db283e47b290fde
kalelfleith <kalelfleith03@gmail.com>
2022-01-19 23:51:06 -0300

    application.properties

---
+++ backend/src/main/groovy/com/groovyboot/domain/base/BaseEntity.groovy
@@ -0,0 +1,32 @@
+package com.groovyboot.domain.base
+
+import com.sun.istack.NotNull
+import com.sun.istack.Nullable
+import groovy.transform.Sortable
+
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.MappedSuperclass
+
+@Sortable
+@MappedSuperclass
+abstract class BaseEntity implements Comparable<BaseEntity> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id
+
+    @Nullable
+    Date lastUpdate
+
+    @NotNull
+    Date dateCreated
+
+    @NotNull
+    Boolean deleted = false
+
+    @Override int compareTo(BaseEntity o){
+        return a <=> o.a
+    }
+}
